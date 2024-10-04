b58e061e403f9194ea0f7f5e1b4462d145896785
AndreVsc <95365578+AndreVsc@users.noreply.github.com>
2024-10-04 09:47:31 -0300

    done

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     build: {
-    rollupOptions: {
-      external: ['axios']
+    roptimizeDeps: {
+      include: ['axios']
     }
+    
   }
 });
